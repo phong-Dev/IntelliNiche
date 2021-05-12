26763a75d5aee2860866815fbbf9392b06a73929
DHP-coder <phong.dh.59cntt@ntu.edu.vn>
2021-05-12 07:51:48 +0700

    test

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ const About = () => {
     return (
         <div>
             about
+            lam about
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iure velit temporibus veritatis nam consectetur, quae tempore, qui impedit repudiandae omnis quis debitis illum corporis nostrum, error exercitationem eligendi? Assumenda.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iure velit temporibus veritatis nam consectetur, quae tempore, qui impedit repudiandae omnis quis debitis illum corporis nostrum, error exercitationem eligendi? Assumenda.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iure velit temporibus veritatis nam consectetur, quae tempore, qui impedit repudiandae omnis quis debitis illum corporis nostrum, error exercitationem eligendi? Assumenda.</p>
