a0e92ea7d4e7a46a75b74869358b68afd6d29083
DHP-coder <phong.dh.59cntt@ntu.edu.vn>
2021-05-12 13:39:24 +0700

    header service

--- pages/service.js
+++ pages/service.js
@@ -1,8 +1,26 @@
+import Head from "next/head";
+
 const Service = () => {
     return (
-        <div>
-            Service
-        </div>
+        <>
+      <Head>
+        <title>Service</title>
+      </Head>
+      {/* -----------header--------- */}
+      <header id="service">
+        <div className="row banner">
+          <div className="banner-text">
+            <h1>We build digital products that power business</h1>
+            <p>
+              Our focus is the agile development of web, cloud, and mobile
+              applications - and helping organizations create and sustain
+              digital innovation
+            </p>
+          </div>
+          </div>
+      </header>
+      {/* --------end header------------- */}
+      </>
     )
 }
 
