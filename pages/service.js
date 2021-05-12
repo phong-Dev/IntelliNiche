0c8cd6612abb4efce8d9cdf875b082a87381c16e
DHP-coder <phong.dh.59cntt@ntu.edu.vn>
2021-05-12 15:26:00 +0700

    mobile and design service

--- pages/service.js
+++ pages/service.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
+import Image from "next/image";
+import { Button } from "../components/Button";
+import { useState } from "react";
 
 const Service = () => {
-    return (
-        <>
+  const [button, setButton] = useState(true);
+  return (
+    <>
       <Head>
         <title>Service</title>
       </Head>
@@ -17,11 +21,105 @@ const Service = () => {
               digital innovation
             </p>
           </div>
-          </div>
+        </div>
       </header>
       {/* --------end header------------- */}
-      </>
-    )
-}
+      {/* --------mobile---------------- */}
+      <section className="service-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <div className="service-content mt-3">
+                <div className="number-service">
+                <Image
+                src="/picture/03.png"
+                alt="03"
+                width={250}
+                height={180}
+              />
+                </div>
+                <div className="info-service">
+                  <h3>Mobile development next-level apps</h3>
+                  <p>
+                    We build innovation IOS and Android applications for a
+                    variety of industries and use-cases
+                  </p>
+                  <div className="service-button">{button && (
+                    <Button buttonStyle="btn--black">IOS development</Button>
+                  )}
+                  {button && (
+                    <Button buttonStyle="btn--black">
+                      Android development
+                    </Button>
+                  )}</div>
+                  
+                  <p>Read more</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5 text-center">
+              <Image
+                src="/picture/mobile.svg"
+                alt="mobile"
+                width={424}
+                height={334}
+                className="img-service"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ---------end mobile----------- */}
+      {/* --------design---------------- */}
+      <section className="service-section">
+        <div className="container">
+          <div className="row">
+            
+            <div className="col-md-5 text-center d-flex">
+              <Image
+                src="/picture/design.svg"
+                alt="mobile"
+                width={471}
+                height={257}
+                className="img-service"
+              />
+            </div>
 
-export default Service
+            <div className="col-md-7">
+              <div className="service-content mt-3">
+                <div className="number-service">
+                <Image
+                src="/picture/04.png"
+                alt="04"
+                width={250}
+                height={180}
+              />
+                </div>
+                <div className="info-service">
+                  <h3>Mobile development next-level apps</h3>
+                  <p>
+                    We build innovation IOS and Android applications for a
+                    variety of industries and use-cases
+                  </p>
+                  <div className="service-button">{button && (
+                    <Button buttonStyle="btn--black">IOS development</Button>
+                  )}
+                  {button && (
+                    <Button buttonStyle="btn--black">
+                      Android development
+                    </Button>
+                  )}</div>
+                  
+                  <p>Read more</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ---------end design----------- */}
+    </>
+  );
+};
+
+export default Service;
